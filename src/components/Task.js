f018b17d23364843a7d8685be06c09502d00e936
annpermi <perminova.anna85@gmail.com>
2022-01-31 12:55:11 -0500

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -45,6 +45,7 @@ export default function Task({
           value={title}
           readOnly={true}
           placeholder="Input title"
+          style={{ background: "red" }}
         />
       </div>
 
